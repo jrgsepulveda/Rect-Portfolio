5a313ceccc50fb3933e39f66e315c8e59528f356
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-11-22 20:21:03 -0500

    Updates

---
+++ precache-manifest.0f1c40a57c38ffe4e1508317bbb57820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6e536a3facfb9b7f7b871c2f1047c3",
+    "url": "/Rect-Portfolio/index.html"
+  },
+  {
+    "revision": "b14671f230379bb5a86d",
+    "url": "/Rect-Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8558a35717f12a384655",
+    "url": "/Rect-Portfolio/static/js/2.11b44f56.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Rect-Portfolio/static/js/2.11b44f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14671f230379bb5a86d",
+    "url": "/Rect-Portfolio/static/js/main.2e4116a8.chunk.js"
+  },
+  {
+    "revision": "b73118b43a416d9d91fd",
+    "url": "/Rect-Portfolio/static/js/runtime-main.ff24b6a4.js"
+  }
+]);
