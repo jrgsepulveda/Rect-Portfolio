77e9bcf0a41f208ab16eccd1f23b8ab912b0b686
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-11-22 20:30:17 -0500

    trying to fix the JSON file not laoding

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={{profilepic}} alt="Jorge Sepulveda Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Jorge Sepulveda Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
