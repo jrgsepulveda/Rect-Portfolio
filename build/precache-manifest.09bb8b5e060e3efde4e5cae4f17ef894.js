2c9c5e83866785a21abb663b5fc824d43a0b1741
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-11-22 20:34:30 -0500

    trying to fix the JSON file not laoding

--- build/precache-manifest.09bb8b5e060e3efde4e5cae4f17ef894.js
+++ build/precache-manifest.09bb8b5e060e3efde4e5cae4f17ef894.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f6e536a3facfb9b7f7b871c2f1047c3",
+    "revision": "ff24b768e649609b65f7899557a974ff",
     "url": "/Rect-Portfolio/index.html"
   },
   {
-    "revision": "b14671f230379bb5a86d",
+    "revision": "4c8a9a3c87117c160f61",
     "url": "/Rect-Portfolio/static/css/main.5facb584.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Rect-Portfolio/static/js/2.11b44f56.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b14671f230379bb5a86d",
-    "url": "/Rect-Portfolio/static/js/main.2e4116a8.chunk.js"
+    "revision": "4c8a9a3c87117c160f61",
+    "url": "/Rect-Portfolio/static/js/main.0ec86eed.chunk.js"
   },
   {
     "revision": "b73118b43a416d9d91fd",
